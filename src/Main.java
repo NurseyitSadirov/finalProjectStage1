9e5214d8b65dbba8871a1ec1817bd7aab1efc29b
polzovatel <nursejitsadirov@gmail.com>
2022-12-16 21:07:09 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+import enums.Gender;
+import model.User;
+import service.serviceimpl.BookServiceImpl;
+import service.serviceimpl.UserServiceImpl;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,9 +18,45 @@ public class Main {
         // Китептин баасы терс сан болбошу керек. Болбосо NegativeNumberException ыргытсын.
         // Китептин чыккан жылы келечек убакыт болбошу керек.Болбосо DateTimeException ыргытсын.
         // Китептин автору бош болбошу керек. Болбосо EmptyStackException ыргытсын.
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
+        Map<Integer,User> userMap = new HashMap<>();
+        userMap.put(11,new User(1111,"Kurrtan","Erkinbaev","@gmail.com","+996990009302", Gender.valueOf(scanner.nextLine()), BigDecimal.valueOf(scanner.nextInt())));
+        userMap.put(22,new User(1111,"Nurseyit","Sadirov","@gmail.com","+996990009302", Gender.valueOf(scanner.nextLine()), BigDecimal.valueOf(scanner.nextInt())));
+        userMap.put(33,new User(1111,"Baiysh","Orozaliev","@gmail.com","+996990009302", Gender.valueOf(scanner.nextLine()), BigDecimal.valueOf(scanner.nextInt())));
+        userMap.put(44,new User(1111,"Baytik","Taalaibekov","@gmail.com","+996990009302", Gender.valueOf(scanner.nextLine()), BigDecimal.valueOf(scanner.nextInt())));
+        userMap.put(55,new User(1111,"Muhammed","Toichubai uulu","@gmail.com","+996990009302", Gender.valueOf(scanner.nextLine()), BigDecimal.valueOf(scanner.nextInt())));
+        BookServiceImpl bookService = new BookServiceImpl();
+        UserServiceImpl service = new UserServiceImpl();
+        System.out.println(userMap);
+        while (true){
 
 
-        
+         switch (scanner1.nextInt()){
+             case 1:
+                 break;
+             case 2:
+                 System.out.println(bookService.getAllBooks());
+                 break;
+             case 3:
+                 break;
+             case 4:
+                 break;
+             case 5:
+                 break;
+             case 6:
+                 break;
+             case 7:
+                 break;
+             case 8:
+                 break;
+             case 9:
+                 break;
+             case 10:
+                 break;
+         }
 
     }
+}
 }
